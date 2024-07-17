5c56aac88b6b68f023bbec604c5693ea6645be96
Phil Hendricks <pfhendri1@gmail.com>
2024-07-16 21:58:38 -0500

    filtering down to only currnet gamgin year

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,8 +5,8 @@ class AdminController < ApplicationController
     @gaming_year = GamingYear.where(active: true).last
     @user_count = User.count
     @virtual_user_count = User.where(attending_virtually: true).count
-    @attending_users_count = User.all.includes(:seats).filter{  |user| user.seats.count.positive? }.count
+    @attending_users_count = User.all.includes(seats: [:event]).where(event: { gaming_year_id: @gaming_year.id }).filter{  |user| user.seats.count.positive? }.count
 
-    @grouped_games = Event.group(:game_type).count
+    @grouped_games = Event.where(gaming_year: @gaming_year).group(:game_type).count
   end
 end
